15b9fb53f8c2a74b106586fec28a90d0bdde6ca8
Yash8092-code <thakuryash.xite@gmail.com>
2025-09-27 17:13:57 +0530

    fix: bind to 0.0.0.0 for render

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ io.on("connection", (socket) => {
 
 // --- Start server ---
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
